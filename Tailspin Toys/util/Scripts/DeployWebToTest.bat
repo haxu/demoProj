342eb501d67124d28719281ab974a4746dacfd06
Brian Keller <Brian Keller>
2015-11-05 00:02:44 -0800

    FF demo

--- Tailspin Toys/util/Scripts/DeployWebToTest.bat
+++
@@ -1,3 +0,0 @@
-C:\Windows\System32\Inetsrv\appcmd.exe recycle apppool Tailspin
-"C:\Program Files\IIS\Microsoft Web Deploy\msdeploy.exe" -verb:sync -source:package=%1 -dest:auto
-pause
--- Tailspin Toys/util/Scripts/readme.txt
+++
@@ -1,1 +0,0 @@
-This directory needs to go into C:\Scripts.
